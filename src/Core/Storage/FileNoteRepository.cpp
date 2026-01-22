493621d6f443e3020f97245eb5a55596a25f742b
Jay Yadav <157211065+jayyadav180@users.noreply.github.com>
2026-01-22 23:24:53 +0530

    Directory addition

--- src/Core/Storage/FileNoteRepository.cpp
+++ src/Core/Storage/FileNoteRepository.cpp
@@ -1,3 +1,28 @@
 #include <filesystem>
-#include "FileNoteRepository.h"
+#include "../Storage/FileNoteRepository.h"
+#include <fstream>
 
+namespace fs = std::filesystem;
+
+FileNoteRepository::FileNoteRepository(const fs::path& basepath): m_basepath(basepath)
+{
+
+}
+
+bool FileNoteRepository::ensuremasterfolderexists()
+{
+    try 
+    {
+        fs::create_directories(m_basepath);
+        return true;
+    }
+    catch (const fs::filesystem_error&) 
+    {
+        return false;
+    }
+}
+
+bool FileNoteRepository::create(const Note& note)
+{
+    
+}
--- src/Core/Storage/FileNoteRepository.h
+++ src/Core/Storage/FileNoteRepository.h
@@ -2,11 +2,21 @@
 
 #include "../Storage/NoteRepository.h"
 
+#include <filesystem>
+#include <optional>
+#include <vector>
+#include <string>
 class FileNoteRepository : public NoteRepository
 {
+    public:
+explicit FileNoteRepository(const std::filesystem::path& basepath);
 bool create(const Note& note) override;
 bool save(const Note& note) override;
 std::optional<Note> load(const std::string& noteid) override;
 std::vector<Note> loadAll() override;
 bool remove(const std::string& noteid) override;
+private:
+bool ensuremasterfolderexists();
+private:
+std::filesystem::path m_basepath;
 };
